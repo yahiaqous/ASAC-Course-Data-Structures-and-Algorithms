28cd16efbf8b9a28446335c6dcb785fcc3011fba
Yahia Qous <yahiaqous@gmail.com>
2021-08-07 14:17:33 +0300

    CC05 Done

--- python/code_challenges/linked-list/linked_list/linked_list.py
+++ python/code_challenges/linked-list/linked_list/linked_list.py
@@ -10,19 +10,44 @@ class LinkedList():
 
     def __init__(self):
         self.head = None
+
     def insert(self, value):
         node = Node(value)
         if self.head:
             node.next=self.head
         self.head = node
 
-    def some_method(self):
-        pass
+    def includes(self,value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
 
     def __str__(self):
-        return str(self.head)
+        current = self.head
+        string = ''
+
+        while current:
+            string+=str(current)+' -> '
+            current=current.next
+        string+="None"
+        return string
 
 if __name__ == "__main__":
-    first_instant = LinkedList('hello')
-    print (str(first_instant))
-    print(first_instant.next)
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    linked_list = LinkedList()
+    linked_list.head = node1
+    node1.next=node2
+    node2.next=node3
+    node3.next=node4
+    node4.next=node5
+    linked_list.insert(6)
+    print(linked_list)
+    print(linked_list.includes(7))
+
--- python/code_challenges/linked-list/tests/test_linked_list.py
+++ python/code_challenges/linked-list/tests/test_linked_list.py
@@ -1,15 +1,64 @@
 import pytest
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
 
 def test_import():
     assert LinkedList
 
+def test_instantiate_empty_list():
+    linked_list=LinkedList()
+    assert linked_list
+
 
 def test_insert():
     linked_list = LinkedList()
-    with pytest.raises(AttributeError):
-        linked_list.head.value
     linked_list.insert("first")
     actual = linked_list.head.value
     assert actual == "first"
+
+def test_head():
+    node1=Node(5)
+    linked_list = LinkedList()
+    linked_list.head=node1
+    assert linked_list.head == node1
+
+def test_insert_multiple_nodes():
+    node1=Node(1)
+    linked_list = LinkedList()
+    linked_list.head=node1
+    linked_list.insert(4)
+    assert linked_list.head.value == 4
+    linked_list.insert(6)
+    assert linked_list.head.value == 6
+    linked_list.insert(9)
+    assert linked_list.head.value == 9
+
+def test_includes():
+    node1=Node(1)
+    node2=Node(2)
+    node3=Node(3)
+    node4=Node(4)
+    linked_list = LinkedList()
+    linked_list.head=node1
+    node1.next=node2
+    node2.next=node3
+    node3.next=node4
+    assert linked_list.includes(1)
+    assert linked_list.includes(2)
+    assert linked_list.includes(3)
+    assert linked_list.includes(4)
+    assert linked_list.includes(5)==False
+    assert linked_list.includes(6)==False
+
+def test_values_exist_in_the_linked_list():
+    node1=Node(1)
+    node2=Node(2)
+    node3=Node(3)
+    node4=Node(4)
+    linked_list = LinkedList()
+    linked_list.head=node1
+    node1.next=node2
+    node2.next=node3
+    node3.next=node4
+    assert linked_list.__str__() == "1 -> 2 -> 3 -> 4 -> None"
+
